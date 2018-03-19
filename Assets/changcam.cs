9364cd6d3d8694126874f848e7a45a99ea2fc424
Chuc <belongbutter@gmail.com>
2018-03-19 14:57:09 +0800

    init

---
+++ Assets/changcam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changcam : MonoBehaviour
+{
+
+
+    public GameObject cam1, cam2; //兩個不同的攝影機
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        cam2.SetActive(false);
+        cam1.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("z") == true)
+        {
+            //若是按下鍵盤的z則切換成第二部攝影機
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+
+        }
+        else if (Input.GetKey("x") == true)
+        {
+            //若是按下鍵盤的x則切換成第一部攝影機
+            cam2.SetActive(false);
+            cam1.SetActive(true);
+        }
+    }
+}
