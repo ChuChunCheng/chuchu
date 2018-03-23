2c7c1df196a126473f5eee404a8af23611df43d9
chuc <belongbutter@gmail.com>
2018-03-23 12:01:53 +0800

    init

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class NewBehaviourScript : MonoBehaviour {
     Rigidbody rb;
+
+    public Text  countText;
+    public Text  timeText;
+    public Text  golText;
+
+    int count;
+    DateTime curr;
     public float speed;
 	// Use this for initialization
 	void Start () {
 
 
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+        countText.text = "分數";
+        golText.text = "";
+
+        curr = DateTime.Now;
+        timeText.text = "60";
 	}
 	
 	// Update is called once per frame
@@ -19,5 +35,28 @@ public class NewBehaviourScript : MonoBehaviour {
         //transform.Translate(x, 0, z);
 
         rb.AddForce(new Vector3(x, 0, z)*speed);
+
+        TimeSpan ts = DateTime.Now - curr;
+        if (ts.Seconds < 60){
+        
+        timeText.text = (60 - ts.Seconds).ToString();
+        }else{
+            timeText.text = "0";
+            golText.text = "You lose!";
+        }
 	}
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+        count++;
+        countText.text = "分數:" + count.ToString();
+
+        if (count >= 10)
+        {
+            golText.text = "You win!";
+        }
+        
+
+
+    }
 }
