848aec5cdca099fb96226f4f05058bbd7cd501ae
chuch <belongbutterr@gmail.com>
2018-03-30 11:58:51 +0800

    new

---
+++ Assets/cmd.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cmd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+
+    public void doPlay()
+    {
+        Debug.Log("doPlay");
+        SceneManager.LoadScene("0316");
+    }
+
+    public void doExit()
+    {
+        Debug.Log("doExit");
+        Application.Quit();
+    }
+}
